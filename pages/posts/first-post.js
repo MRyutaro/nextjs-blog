c8dbddff80e25108eb33cb12e9a479e54122e51d
MRyutaro <m2002r1028@icloud.com>
2023-08-04 03:18:33 +0900

    add: css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,11 @@
 import Link from 'next/link'
 import Head from 'next/head'
 import Script from 'next/script'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
     return(
-        <>
+        <Layout>
             <Head>
                 <title>
                     First Post!
@@ -20,6 +21,6 @@ export default function FirstPost() {
             />
             <h1>Hello</h1>
             <Link href={"/"}>Home</Link>
-        </>
+        </Layout>
     )
 }
